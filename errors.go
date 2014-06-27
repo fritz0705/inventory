4541001a4b41cb925751a8822e392ba198804d98
Fritz Grimpen <fritz@grimpen.net>
2014-06-27 16:21:27 +0000

    Implement generic error handler

--- errors.go
+++ errors.go
@@ -2,9 +2,15 @@ package inventory
 
 import (
 	"net/http"
+	"log"
 )
 
 func (app *Application) NotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO(fritz): Implement pretty NotFound handler
 	http.NotFound(w, r)
+}
+
+func (app *Application) Error(w http.ResponseWriter, err error) {
+	log.Fatal(err)
+	http.Error(w, err.Error(), http.StatusInternalServerError)
 }
