c76851db743e0edd1c78517b9b0991647ce2f96d
Fritz Grimpen <fritz@grimpen.net>
2014-08-08 18:53:33 +0000

    Add short list of electronic distributors

---
+++ distributors.sql
@@ -0,0 +1,4 @@
+INSERT INTO 'distributor' ('name', 'url', 'homepage', 'api') VALUES
+	('Reichelt Elektronik', 'http://reichelt.de/?ARTICLE=%v', 'http://reichelt.de/', 'reichelt'),
+	('Farnell DE', 'http://de.farnell.com/x/dp/%v', 'http://de.farnell.com/', NULL),
+	('Pollin', 'http://www.pollin.de/shop/dt/%v', 'http://pollin.de/', NULL);
